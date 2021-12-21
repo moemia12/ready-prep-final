04c22af60ea0c7d04486eb05500d91bc6121ae15
Mohsin Miah <moemia12@gmail.com>
2021-12-21 17:14:20 +0000

    Folder Updates

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
+      <Text>Testd</Text>
       <StatusBar style="auto" />
     </View>
   );
