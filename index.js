28a4335d858f593aada1447ca158ca18d64b74e6
Mohsin Miah <moemia12@gmail.com>
2021-12-21 16:16:04 +0000

    File updates

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {main as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
